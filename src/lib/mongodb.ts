1f88207df38d9fa96d259149e9735e1d305a7fd0
tinhtam221986 <tinhtam221986@gmail.com>
2025-12-10 11:58:38 +0700

    Update mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,30 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Chưa điền MONGODB_URI trong Vercel đâu bác ơi!');
+  throw new Error("Chưa có MONGODB_URI trong biến môi trường!");
 }
 
+// Cái này để giúp Vercel không bị quá tải kết nối
 let cached = (global as any).mongoose;
 
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  if (cached.conn) return cached.conn;
+// ĐÂY LÀ CÁI HÀM MÀ VERCEL ĐANG TÌM KIẾM 👇
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
     };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
       return mongoose;
     });
   }
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-export default dbConnect;
