9925861ffe8d468d53d13c313b6671e8a0b7c0a5
tinhtam221986 <tinhtam221986@gmail.com>
2025-12-22 14:33:06 +0000

    fix next config env

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
-      R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
-      R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
-      R2_BUCKET_NAME: process.env.R2_BUCKET_NAME,
-      R2_ENDPOINT: process.env.R2_ENDPOINT,
-      R2_PUBLIC_URL: process.env.R2_PUBLIC_URL,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.r2.dev',
-        },
-        {
-          protocol: 'https',
-          hostname: '**.r2.cloudflarestorage.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'api.dicebear.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'pbs.twimg.com',
-        },
-      ],
-    },
+  env: {
+    R2_BUCKET_NAME: process.env.R2_BUCKET_NAME,
+    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
+    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
+    R2_PUBLIC_DOMAIN: process.env.R2_PUBLIC_DOMAIN,
+    R2_ENDPOINT: process.env.R2_ENDPOINT,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
