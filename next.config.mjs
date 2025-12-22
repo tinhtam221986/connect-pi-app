f325b9f8b4a5cb7617e1bb56af4fcfbca28d407d
tinhtam221986 <tinhtam221986@gmail.com>
2025-12-22 16:28:29 +0000

    remove env block

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    R2_BUCKET_NAME: process.env.R2_BUCKET_NAME,
-    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
-    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
-    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
-    R2_PUBLIC_DOMAIN: process.env.R2_PUBLIC_DOMAIN,
-    R2_ENDPOINT: process.env.R2_ENDPOINT,
-  },
+  // Đã xóa phần env, chỉ giữ lại phần images
   images: {
     remotePatterns: [
       {
